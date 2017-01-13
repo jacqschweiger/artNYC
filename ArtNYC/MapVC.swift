f4065e945c168c43be872453653f2d230926b06c
jacqschweiger <jacqschweiger@gmail.com>
2017-01-13 16:01:36 -0500

    added mapkit

---
+++ ArtNYC/MapVC.swift
@@ -0,0 +1,32 @@
+//
+//  MapVC.swift
+//  ArtNYC
+//
+//  Created by Jacqueline Minneman on 1/13/17.
+//  Copyright © 2017 Jacqueline Schweiger. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+class MapVC: UIViewController {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    let regionRadius: CLLocationDistance = 1000
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let initialLocation = CLLocation(latitude: 40.7829, longitude: -73.9654)
+        centerMapOnLocation(location: initialLocation)
+        
+    }
+    
+    
+    func centerMapOnLocation(location: CLLocation) {
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(location.coordinate,
+                                                                  regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+}
