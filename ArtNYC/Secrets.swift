f827957847425a3de6064fd5caa940c98c72d805
jacqschweiger <jacqschweiger@gmail.com>
2017-01-14 16:52:17 -0500

    added gitignore

---
+++ ArtNYC/Secrets.swift
@@ -0,0 +1,15 @@
+//
+//  Secrets.swift
+//  ArtNYC
+//
+//  Created by Jacqueline Minneman on 1/14/17.
+//  Copyright © 2017 Jacqueline Schweiger. All rights reserved.
+//
+
+import Foundation
+
+struct Secrets {
+
+    static let key: String = "AIzaSyCbSMc7W4GMkAJTZuv1ai03wDvrn5-OGFA"
+
+}
