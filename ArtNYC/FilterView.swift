bebf4d55368718bfb50b11d1f1eaa33616bf2d97
jacqschweiger <jacqschweiger@gmail.com>
2017-01-31 10:10:46 -0500

    added filter title

--- ArtNYC/FilterView.swift
+++ ArtNYC/FilterView.swift
@@ -19,18 +19,33 @@ class FilterView: UIView {
         self.backgroundColor = UIColor(white: 1, alpha: 0.75)
         
         self.addSubview(filterMenu)
-        
         self.filterMenu.backgroundColor = UIColor.blue
         self.filterMenu.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
         self.filterMenu.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-        self.filterMenu.heightAnchor.constraint(equalToConstant: 50).isActive = true
-        self.filterMenu.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        self.filterMenu.heightAnchor.constraint(equalTo: self.heightAnchor, multiplier: 0.7).isActive = true
+        self.filterMenu.widthAnchor.constraint(equalTo: self.widthAnchor, multiplier: 0.7).isActive = true
         self.filterMenu.translatesAutoresizingMaskIntoConstraints = false
+        
+        setUpElements()
     
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setUpElements(){
+        let titleLabel = UILabel()
+        self.filterMenu.addSubview(titleLabel)
+        titleLabel.backgroundColor = UIColor.red
+        titleLabel.text = "Filter"
+        titleLabel.topAnchor.constraint(equalTo: self.filterMenu.topAnchor).isActive = true
+        titleLabel.widthAnchor.constraint(equalTo: self.filterMenu.widthAnchor).isActive = true
+        titleLabel.leftAnchor.constraint(equalTo: self.filterMenu.leftAnchor).isActive = true
+        titleLabel.heightAnchor.constraint(equalTo: self.filterMenu.heightAnchor, multiplier: 0.1).isActive = true
+        titleLabel.textAlignment = .center
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        
     }
     
     
