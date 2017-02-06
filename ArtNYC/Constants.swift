f8de9aadb3933650f804e62b24f93c1a78db44bb
jacqschweiger <jacqschweiger@gmail.com>
2017-02-06 18:55:52 -0500

    changed color

--- ArtNYC/Constants.swift
+++ ArtNYC/Constants.swift
@@ -11,10 +11,9 @@ import UIKit
 
 extension UIColor {
     enum ColorName: Int {
-        case oatmeal = 0xdddfd4ff
         case grey = 0xF2F1EFff
-        case yellow = 0xfae596ff
-        case turquoise = 0x3fb0acff
+        //case turquoise = 0x3fb0acff
+        case turquoise = 0x4183D7ff
         case darkBlue = 0x173e43ff
     }
 }
